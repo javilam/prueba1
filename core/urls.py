f124ccc2edd37d7cf8ba58299578bad9f39e4073
javilam <javilam@gmail.com>
2022-07-30 01:16:42 -0400

    vista blog

--- core/urls.py
+++ core/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from argparse import Namespace
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('blog/',include('blog.urls', namespace='blog'))
 ]
